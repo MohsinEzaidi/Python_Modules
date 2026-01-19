ba0c2019f5c3c595d911e649f2fc486f874f8ee5
Mohsine Ezzaidi <149973376+MohsinEzaidi@users.noreply.github.com>
2026-01-19 05:13:36 +0000

    ..

---
+++ test.py
@@ -0,0 +1,21 @@
+x = 0
+class Plant:
+    def __init__(self, name):
+        self.__name = name
+    def get_name(self):
+        return self.__name
+    def set_name(self, new_name):
+        self.__name = new_name
+
+class Flower(Plant):
+    def __init__(self, name, color):
+        super().__init__(name)
+        self.__color = color
+    def get_color(self):
+        return self.__color
+    def set_color(self, new_color):
+        self.color = new_color
+
+p = Plant('5izo')
+f = Flower('chabba warda', '7amra ya 5oya 7amra')
+print(p._Plant__name)
