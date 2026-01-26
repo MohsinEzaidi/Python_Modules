13a0312a3847a22eb405012fc67397e3ae6ab97c
MohsinEzaidi <mohsinezzaidi@gmail.com>
2026-01-26 09:49:37 +0100

    module_03

---
+++ module_03/ex0/ft_command_quest.py
@@ -0,0 +1,21 @@
+from sys import argv as av
+
+
+def command_quest() -> None:
+    try:
+        ac = av.__len__()
+        if ac == 1:
+            print('No arguments provided!')
+        print(f'Program name: {av[0]}')
+        if ac > 1:
+            print(f'Arguments received: {ac - 1}')
+            for i in range(1, ac):
+                print(f'Argument {i}: {av[i]}')
+        print(f'Total arguments: {ac}')
+    except Exception as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    print('=== Command Quest ===')
+    command_quest()
--- test.py
+++ test.py
@@ -1,8 +1,1 @@
-class ErrorS(Exception):
-    def __init__(self, msg: str = 'hahahahahahahaha'):
-        super().__init__(msg)
-
-try:
-    raise ValueError(['what', 1, 0.9, {'j': 55}])
-except Exception as e:
-    print(e.__class__.__name__)
+from sys import argv as av
